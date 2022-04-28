b1c252de5b2fed7152cdbe8a888db2d41785ed7f
guilhermeamorim <guilhermeamorim@deso-se.com.br>
2022-04-28 13:53:05 -0300

    React: function for click filter button

--- src/pages/cardapio/filtros/index.tsx
+++ src/pages/cardapio/filtros/index.tsx
@@ -1,13 +1,21 @@
+import React from 'react';
 import filtros from './filtros.json';
 import StyleFiltros from './Filtros.module.scss'
 
 type IOpcao = typeof filtros[0];
 
-export default function Filtros() {
-    function selecionarFiltros(opcao: IOpcao) {}
+interface Props {
+    filtro: number | null;
+    setFiltro: React.Dispatch<React.SetStateAction<number | null>>
+  }
+
+export default function Filtros({ filtro, setFiltro }: Props) {
+    function selecionarFiltros(opcao: IOpcao) {
+        return setFiltro(opcao.id);
+    }
     return <div className={StyleFiltros.filtros}>
         {filtros.map((opcao) => (
-            <button className={StyleFiltros.filtros__filtro} key={opcao.id} onClick={() => selecionarFiltros(opcao)}>
+            <button className={`${StyleFiltros.filtros__filtro} ${filtro === opcao.id ? StyleFiltros["filtros__filtro--ativo"] : ""}`} key={opcao.id} onClick={() => selecionarFiltros(opcao)}>
                 {opcao.label}
             </button>
         ))}
