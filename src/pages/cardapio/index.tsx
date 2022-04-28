733ed3a66d32e8a366b7465af8329085aa5e1775
Guilherme Amorim <guilherme.o.a.ufal@gmail.com>
2022-04-27 23:37:54 -0300

    feat: settings to insert images

--- src/pages/cardapio/index.tsx
+++ src/pages/cardapio/index.tsx
@@ -1,8 +1,11 @@
+import StyleCardapio from './Cardapio.module.scss';
+import {ReactComponent as Logo} from 'assets/logo.svg'
+
 export default function Cardapio() {
     return (
         <main>
-            <nav className="menu">
-
+            <nav className={StyleCardapio.menu}>
+                <Logo />
             </nav>
         </main>
     )
