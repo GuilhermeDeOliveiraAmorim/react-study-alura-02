36277bda3ea42dad7b348f886ef26b41f50320c3
Guilherme Amorim <guilherme.o.a.ufal@gmail.com>
2022-04-29 19:48:48 -0300

    feat: mporting the menu items and inserting them into the Item component

--- src/pages/cardapio/itens/item/index.tsx
+++ src/pages/cardapio/itens/item/index.tsx
@@ -1,30 +1,37 @@
 import logo from "assets/logo.svg";
 import StyleItem from "./Item.module.scss";
+import cardapio from "../itens.json";
 
-export default function Item() {
+type Props = typeof cardapio[0];
+
+export default function Item(props: Props) {
+    const { title, description, category, size, serving, price } = props;
     return (
         <div className={StyleItem.item}>
             <div className={StyleItem.item__imagem}>
-                <img src={logo} alt="imagem" />
+                <img src={logo} alt={title} />
             </div>
             <div className={StyleItem.item__descricao}>
                 <div className={StyleItem.item__titulo}>
                     <h2>
-                        Comida
+                        {title}
                     </h2>
                     <p>
-                        Descrição
+                        {description}
                     </p>
                 </div>
                 <div className={StyleItem.item__tags}>
                     <div className={StyleItem.item__tipo}>
-                        Tipo
+                        {category.label}
                     </div>
                     <div className={StyleItem.item__qtdpessoas}>
-                        400g
+                        {size}g
+                    </div>
+                    <div className={StyleItem.item__porcao}>
+                        Serve até {serving} pessoa{serving === 1 ? '' : 's'}
                     </div>
                     <div className={StyleItem.item__valor}>
-                        R$ Valor
+                        R$ {price.toFixed(2)}
                     </div>
                 </div>
             </div>
