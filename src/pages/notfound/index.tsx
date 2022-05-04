537f451302f4fd5189fd48bd1de33de1d538eda2
Guilherme Amorim <guilherme.o.a.ufal@gmail.com>
2022-05-04 06:03:28 -0300

    feat: back button on error page

--- src/pages/notfound/index.tsx
+++ src/pages/notfound/index.tsx
@@ -2,13 +2,20 @@ import StylesNotFound from './NotFound.module.scss';
 import { ReactComponent as NotFoundImage } from 'assets/not_found.svg';
 import classNames from 'classnames';
 import StylesTema from 'styles/Tema.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 export default function NotFound() {
+	const navigate = useNavigate();
 	return(
 		<div className={classNames({
 			[StylesNotFound.container]: true,
 			[StylesTema.container]: true
 		})}>
+			<div className={StylesNotFound.voltar}>
+				<button onClick={() => navigate(-1)}>
+					{'< Voltar'}
+				</button>
+			</div>
 			<NotFoundImage style={{height:'300px'}} />
 		</div>
 	);
