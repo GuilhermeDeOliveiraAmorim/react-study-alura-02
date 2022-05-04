70928630f26d51ac9b9ecf22afa3f875e117381f
guilhermeamorim <guilhermeamorim@deso-se.com.br>
2022-05-04 13:27:58 -0300

    feat:  using useNavigate to access dishes via id

--- src/pages/inicio/index.tsx
+++ src/pages/inicio/index.tsx
@@ -2,10 +2,15 @@ import cardapio from 'data/cardapio.json';
 import StyleInicio from './Inicio.module.scss';
 import StyleTema from 'styles/Tema.module.scss';
 import nossaCasa from 'assets/nossa_casa.png';
+import { useNavigate } from 'react-router-dom';
 
 export default function Inicio() {
 	let pratosRecomendados = [...cardapio];
 	pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).splice(0, 3);
+	const navigate = useNavigate();
+	function redirecionarParaDetalhes(prato: typeof cardapio[0]) {
+		navigate(`/prato/${prato.id}`, { state: { ...prato } });
+	}
 	return (
 		<section>
 			<h3 className={StyleTema.titulo}>
@@ -17,7 +22,10 @@ export default function Inicio() {
 						<div className={StyleInicio.recomendado__imagem}>
 							<img src={item.photo} alt={item.title} />
 						</div>
-						<button className={StyleInicio.recomendado__botao}>
+						<button
+							className={StyleInicio.recomendado__botao}
+							onClick={() => redirecionarParaDetalhes(item)}
+						>
 							Ver mais
 						</button>
 					</div>
