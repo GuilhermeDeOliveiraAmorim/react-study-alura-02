d3ffc845b25f1d8e15e41eccd348ce090e92ed7b
guilhermeamorim <guilhermeamorim@deso-se.com.br>
2022-05-06 13:13:31 -0300

    feat: header tweaks to not show up in NotFound

--- src/pages/prato/index.tsx
+++ src/pages/prato/index.tsx
@@ -1,35 +1,42 @@
 import StylesPrato from './Prato.module.scss';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Route, Routes, useNavigate, useParams } from 'react-router-dom';
 import cardapio from 'data/cardapio.json';
 import TagsPrato from 'components/tagsPrato';
 import NotFound from 'pages/notfound';
+import PaginaPadrao from 'components/paginaPadrao';
 
 export default function Prato() {
 	const { id } = useParams();
+	const navigate = useNavigate();
 	const prato = cardapio.find(item => item.id === Number(id));
 	if (!prato) {
 		return <NotFound />;
 	}
-	const navigate = useNavigate();
 	return (
-		<>
-			<button className={StylesPrato.voltar} onClick={() => navigate(-1)}>
-				{'< Voltar'}
-			</button>
-			<section className={StylesPrato.container}>
-				<h1 className={StylesPrato.titulo}>
-					{prato.title}
-				</h1>
-				<div>
-					<img src={prato.photo} alt={prato.title} />
-				</div>
-				<div className={StylesPrato.conteudo}>
-					<p className={StylesPrato.conteudo__descricao}>
-						{prato.description}
-					</p>
-					<TagsPrato {...prato} />
-				</div>
-			</section>
-		</>
+		<Routes>
+			<Route path="*" element={<PaginaPadrao />}>
+				<Route index element={
+					<>
+						<button className={StylesPrato.voltar} onClick={() => navigate(-1)}>
+							{'< Voltar'}
+						</button>
+						<section className={StylesPrato.container}>
+							<h1 className={StylesPrato.titulo}>
+								{prato.title}
+							</h1>
+							<div>
+								<img src={prato.photo} alt={prato.title} />
+							</div>
+							<div className={StylesPrato.conteudo}>
+								<p className={StylesPrato.conteudo__descricao}>
+									{prato.description}
+								</p>
+								<TagsPrato {...prato} />
+							</div>
+						</section>
+					</>
+				} />
+			</Route>
+		</Routes>
 	);
 }
